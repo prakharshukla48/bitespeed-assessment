8788cb4497fc04bc1088b6c5303d060b2a44b397
shuklaprakhar48 <shuklaprakhar48@gmail.com>
2024-05-30 15:02:18 +0530

    changes to db

--- models.py
+++ models.py
@@ -4,9 +4,15 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
     phone_number = db.Column(db.String(15), unique=True, nullable=True)
-    primary_contact_id = db.Column(db.Integer, nullable=False)
+    linked_id = db.Column(db.Integer, nullable=True)
+    link_precedence =db.Column(db.String(15))
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, email, phone_number, primary_contact_id):
+    def __init__(self, email, phone_number, linked_id, link_precedence, created_at, updated_at):
         self.email = email
         self.phone_number = phone_number
-        self.primary_contact_id = primary_contact_id
+        self.linked_id = linked_id
+        self.link_precedence = link_precedence
+        self.created_at = created_at
+        self.updated_at = updated_at
