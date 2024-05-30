68116380a391d80ead7a8fd5b0f4c21b872ce083
shuklaprakhar48 <shuklaprakhar48@gmail.com>
2024-05-30 16:20:57 +0530

    correction in the logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from models import Contact
 from database import db, init_db
+import datetime
 
 app = Flask(__name__)
 init_db(app)
@@ -10,32 +11,59 @@ def identify():
     data = request.get_json()
     email = data.get('email')
     phone_number = data.get('phoneNumber')
-    primary_contact = None
-    secondary_contacts = set()
 
     if email:
-        primary_contact = Contact.query.filter_by(email=email).first()
-    if not primary_contact and phone_number:
-        primary_contact = Contact.query.filter_by(phone_number=phone_number).first()
+        primary_email = Contact.query.filter_by(email=email).first()
+    if not primary_email and phone_number:
+        primary_phone = Contact.query.filter_by(phone_number=phone_number).first()
+    # print(f"is phone or email present ? {primary_phone}, {primary_email}")
 
-    if primary_contact:
-        primary_contact_id = primary_contact.primary_contact_id
+    if primary_email or primary_phone:
+        if primary_email:
+            if phone_number:
+                primary_contact_id = primary_email.id
+                new_contact = Contact(email=email,
+                                      phone_number=phone_number,
+                                      linked_id=primary_email.id,
+                                      link_precedence= "Secondary",
+                                      created_at= datetime.datetime.now(),
+                                      updated_at= datetime.datetime.now())
+                db.session.add(new_contact)
+                db.session.commit()
+
+        else:
+            if email:
+                primary_contact_id = primary_phone.id
+                new_contact = Contact(email=email,
+                                      phone_number=phone_number,
+                                      linked_id=primary_phone.id,
+                                      link_precedence="Secondary",
+                                      created_at=datetime.datetime.now(),
+                                      updated_at=datetime.datetime.now())
+                db.session.add(new_contact)
+                db.session.commit()
     else:
         primary_contact_id = Contact.query.count() + 1
-        new_contact = Contact(email=email, phone_number=phone_number, primary_contact_id=primary_contact_id)
+        new_contact = Contact(email=email,
+                              phone_number=phone_number,
+                              linked_id=None,
+                              link_precedence="Primary",
+                              created_at=datetime.datetime.now(),
+                              updated_at=datetime.datetime.now())
         db.session.add(new_contact)
         db.session.commit()
 
-    contacts = Contact.query.filter_by(primary_contact_id=primary_contact_id).all()
-    emails = list({c.email for c in contacts if c.email})
-    phone_numbers = list({c.phone_number for c in contacts if c.phone_number})
-    secondary_contact_ids = [c.id for c in contacts if c.id != primary_contact_id]
+    secondary_contacts = Contact.query.filter_by(linked_id=primary_contact_id).all()
+    secondary_contact_ids = [contact.id for contact in secondary_contacts]
+    emails = [contact.email for contact in secondary_contacts] + [Contact.query.get(primary_contact_id).email]
+    phone_numbers = [contact.phone_number for contact in secondary_contacts] + [Contact.query.get(primary_contact_id).phone_number]
+    # secondary_contact_ids = [c.id for c in contacts if c.id != primary_contact_id]
 
     response = {
         "contact": {
             "primaryContactId": primary_contact_id,
-            "emails": emails,
-            "phoneNumbers": phone_numbers,
+            "emails": set(emails),
+            "phoneNumbers": set(phone_numbers),
             "secondaryContactIds": secondary_contact_ids
         }
     }
--- models.py
+++ models.py
@@ -2,12 +2,13 @@ from database import db
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=True)
-    phone_number = db.Column(db.String(15), unique=True, nullable=True)
+    email = db.Column(db.String(120), nullable=True)
+    phone_number = db.Column(db.String(15), nullable=True)
     linked_id = db.Column(db.Integer, nullable=True)
     link_precedence =db.Column(db.String(15))
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
+    deleted_at = db.Column(db.DateTime, nullable=False)
 
     def __init__(self, email, phone_number, linked_id, link_precedence, created_at, updated_at):
         self.email = email
@@ -16,3 +17,4 @@ class Contact(db.Model):
         self.link_precedence = link_precedence
         self.created_at = created_at
         self.updated_at = updated_at
+        self.deleted_at = deleted_at
