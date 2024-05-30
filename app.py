1b5530b33a666a5532328509e37740202b592c2b
shuklaprakhar48 <shuklaprakhar48@gmail.com>
2024-05-30 17:59:56 +0530

    Handled already present data logic

--- app.py
+++ app.py
@@ -17,24 +17,32 @@ def identify():
         primary_email = Contact.query.filter_by(email=email).first()
     if phone_number:
         primary_phone = Contact.query.filter_by(phone_number=phone_number).first()
-    # print(f"is phone or email present ? {primary_phone}, {primary_email}")
 
     if primary_email and primary_phone:
         print("botth email and phone found")
-        linked_id = primary_email.linked_id
-        if not linked_id:
+
+        linked_id_email = primary_email.linked_id
+        linked_id_phone = primary_phone.linked_id
+        if linked_id_email == linked_id_phone == None:
+            primary_phone.linked_id = primary_email.id
+            primary_phone.link_precedence = "Secondary"
+            primary_phone.updated_at = datetime.datetime.now()
+            db.session.commit()
             primary_contact_id = primary_email.id
-        else:
-            primary_contact_id = linked_id
+        elif linked_id_email:
+            primary_contact_id = linked_id_email
+        elif linked_id_phone:
+            primary_contact_id = linked_id_phone
+
     elif primary_email or primary_phone:
         print("Either of them present")
         if primary_email:
+            linked_id = primary_email.linked_id
+            if not linked_id:
+                primary_contact_id = primary_email.id
+            else:
+                primary_contact_id = linked_id
             if phone_number:
-                linked_id = primary_email.linked_id
-                if not linked_id:
-                    primary_contact_id = primary_email.id
-                else:
-                    primary_contact_id = linked_id
                 new_contact = Contact(email=email,
                                       phone_number=phone_number,
                                       linked_id=primary_contact_id,
@@ -42,16 +50,17 @@ def identify():
                                       created_at= datetime.datetime.now(),
                                       updated_at= datetime.datetime.now(),
                                       deleted_at=None)
-                db.query.add(new_contact)
-                db.query.commit()
+                db.session.add(new_contact)
+                db.session.commit()
 
         else:
+
+            linked_id = primary_phone.linked_id
+            if not linked_id:
+                primary_contact_id = primary_phone.id
+            else:
+                primary_contact_id = linked_id
             if email:
-                linked_id = primary_phone.linked_id
-                if not linked_id:
-                    primary_contact_id = primary_phone.id
-                else:
-                    primary_contact_id = linked_id
                 new_contact = Contact(email=email,
                                       phone_number=phone_number,
                                       linked_id=primary_contact_id,
@@ -59,8 +68,8 @@ def identify():
                                       created_at=datetime.datetime.now(),
                                       updated_at=datetime.datetime.now(),
                                       deleted_at=None)
-                db.query.add(new_contact)
-                db.query.commit()
+                db.session.add(new_contact)
+                db.session.commit()
     else:
         primary_contact_id = Contact.query.count() + 1
         new_contact = Contact(email=email,
@@ -70,8 +79,8 @@ def identify():
                               created_at=datetime.datetime.now(),
                               updated_at=datetime.datetime.now(),
                               deleted_at=None)
-        db.query.add(new_contact)
-        db.query.commit()
+        db.session.add(new_contact)
+        db.session.commit()
 
     secondary_contacts = Contact.query.filter_by(linked_id=primary_contact_id).all()
     secondary_contact_ids = [contact.id for contact in secondary_contacts]
